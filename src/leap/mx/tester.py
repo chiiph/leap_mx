1727d72858bf4e7177251ff0ea1bcd77d4455772
Tomás Touceda <chiiph@leap.se>
2013-12-05 16:21:00 -0300

    Add dummy document to tester capabilities

--- src/leap/mx/tester.py
+++ src/leap/mx/tester.py
@@ -1,16 +1,37 @@
+import argparse
 import ConfigParser
 import sys
 import os
+import datetime
 
 from twisted.internet import reactor, defer
 from twisted.python import filepath, log
 
 from leap.mx import couchdbhelper
 from leap.mx.mail_receiver import MailReceiver
+from leap.soledad.common.document import SoledadDocument
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='LEAP MX tester script')
+    parser.add_argument('--process-mail', nargs="?",
+                        metavar="/path/to/email",
+                        dest="mail",
+                        help=("Email to process"))
+    parser.add_argument('--produce-dummy-mail', nargs="?",
+                        metavar="UUID",
+                        dest="uuid",
+                        help=(
+                            "Directly save a dummy email to Soledad. To get "
+                            "the uuid for a user, you might want to run "
+                            "postmap -v -q <email address> tcp:localhost:4242"))
+
+    args = parser.parse_args()
+    if not ((args.mail is not None) ^ (args.uuid is not None)):
+        print "ERROR: Can only set one option"
+        parser.print_help()
+        quit()
+
     log.startLogging(sys.stdout)
-    fullpath = os.path.realpath(sys.argv[1])
 
     log.msg("Starting test for %s..." % (fullpath,))
 
@@ -38,9 +59,32 @@ if __name__ == "__main__":
                                                     port)
 
     mr = MailReceiver(mail_couch_url_prefix, cdb, [])
-    fpath = filepath.FilePath(fullpath)
 
-    d = mr._process_incoming_email(None, fpath, 0)
-    d.addCallback(lambda x: reactor.stop())
+    if args.mail is not None:
+        fullpath = os.path.realpath(args.mail)
+        fpath = filepath.FilePath(fullpath)
+
+        d = mr._process_incoming_email(None, fpath, 0)
+        d.addCallback(lambda x: reactor.stop())
+    elif args.uuid is not None:
+        doc = SoledadDocument(doc_id=str(pyuuid.uuid4()))
+
+        message = """
+From: LEAP MX Tester <noreply@bitmask.net>
+To: Unknown User <noreply-user@bitmask.net>
+Subject: Test dummy mail (%s)
+
+This is an automatically generated dummy email to test connectivity
+between mails saved in Soledad from the LEAP MX daemon.
+
+If you receive this, be happy, things are working!
+        """ % (datetime.datetime.now(),)
+        data = {'incoming': True, 'content': message}
+
+        d = mr._export_message(args.uuid, doc)
+        d.addCallback(lambda x: reactor.stop())
+    else:
+        print "Nothing to do here..."
+        quit()
 
     reactor.run()
